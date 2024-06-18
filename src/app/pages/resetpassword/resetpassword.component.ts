3a0c4e80f3e8b53dc5a9b7abc8e5925998b830c1
dinukrana2000 <dinuk2000rana@gmail.com>
2024-06-18 12:16:09 +0530

    add custom validator password mistmatcer

--- src/app/pages/resetpassword/resetpassword.component.ts
+++ src/app/pages/resetpassword/resetpassword.component.ts
@@ -1,5 +1,5 @@
 import { Component,OnInit } from '@angular/core';
-import { FormGroup,FormBuilder,Validators } from '@angular/forms';
+import { FormGroup,FormBuilder,Validators, ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/service/userservice/auth.service';
 import { ToastrService } from 'ngx-toastr';
@@ -19,11 +19,12 @@ constructor(private fb: FormBuilder,private router:Router,private authService:Au
 
 ngOnInit() {
   this.resetForm = this.fb.group({
-    email:[ localStorage.getItem('resetPasswordEmail'), [Validators.required]],
-    pwd: ['', [Validators.required, Validators.minLength(6),Validators.pattern('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{6,}$'),Validators.maxLength(15)]],
-    confirm_pwd: ['', [Validators.required, Validators.minLength(6),Validators.pattern('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{6,}$'),Validators.maxLength(15)]],
-  });
+    email: [localStorage.getItem('resetPasswordEmail'), [Validators.required]],
+    pwd: ['', [Validators.required, Validators.minLength(6), Validators.pattern('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{6,}$'), Validators.maxLength(15)]],
+    confirm_pwd: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(15)]],
+  }, { validators: passwordMatchValidator });
 }
+
 
   onSubmit() {
     if (this.resetForm.valid) {
@@ -47,3 +48,14 @@ ngOnInit() {
   }
 
 }
+
+ const passwordMatchValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const password = control.get('pwd')?.value;
+  const confirmPassword = control.get('confirm_pwd')?.value;
+
+  if (password && confirmPassword && password !== confirmPassword) {
+    return { passwordMismatch: true };
+  }
+
+  return null;
+};
